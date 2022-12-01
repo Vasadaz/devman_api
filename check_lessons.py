8037aea2449cbece461d16026867f42a870edcbc
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-12-01 21:21:11 +0300

    Update the "get_verification_results" function.

--- check_lessons.py
+++ check_lessons.py
@@ -1,3 +1,4 @@
+import time
 from pprint import pprint
 
 import requests
@@ -15,11 +16,20 @@ def get_lessons(token: str) -> dict:
     return response.json()
 
 
-def get_verification_results(token: str) -> dict:
+def get_verification_results(token: str, timestamp: float) -> dict:
     headers = {'Authorization': f'Token {token}'}
+    params = {
+        'timestamp': timestamp,
+    }
     url = 'https://dvmn.org/api/long_polling/'
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        timeout=120,
+    )
+    print(response.url)
     response.raise_for_status()
 
     return response.json()
@@ -29,10 +39,21 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     token = env.str('DEVMAN_TOKEN')
-
-    lessons = get_lessons(token)
-    pprint(lessons)
+    timestamp = None
 
     while True:
-        verifications = get_verification_results(token)
-        pprint(verifications)
+        try:
+            verifications = get_verification_results(token, timestamp)
+
+            if verifications['status'] == 'found':
+                timestamp = verifications['last_attempt_timestamp']
+                pprint(verifications)
+            else:
+                timestamp = verifications['timestamp_to_request']
+                print('Not Found', timestamp)
+                pprint(verifications)
+        except requests.exceptions.ReadTimeout as error:
+            print(error)
+        except requests.exceptions.ConnectionError as error:
+            print(error)
+            time.sleep(10)
